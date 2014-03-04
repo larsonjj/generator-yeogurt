72c0f197b09fb220a5a5e09433715b910b318ab7
Jake Larson <jake.j.larson@gmail.com>
2014-03-03 23:33:59 -0600

    Added styles generator

---
+++ style/index.js
@@ -0,0 +1,42 @@
+'use strict';
+var util = require('util');
+var yeoman = require('yeoman-generator');
+var fileJSON = require(process.cwd() + '/.yo-rc.json')['generator-yeogurt'].config;
+
+var StyleGenerator = module.exports = function StyleGenerator(args, options, config) {
+    // By calling `NamedBase` here, we get the argument to the subgenerator call
+    // as `this.name`.
+    yeoman.generators.NamedBase.apply(this, arguments);
+
+    // options
+    this.useDashboard = this.options.dashboard || false;
+    this.folder = this.options.folder || false;
+    this.cssOption = fileJSON.cssOption;
+
+    console.log('You called the style subgenerator with the argument ' + this.name + '.');
+};
+
+util.inherits(StyleGenerator, yeoman.generators.NamedBase);
+
+StyleGenerator.prototype.files = function files() {
+    if (!this.name) {
+        console.log('Name cannot be empty. Operation aborted.');
+        return;
+    }
+    if (this.folder) {
+        if (this.cssOption === 'LESS') {
+            this.template('style.less', 'dev/styles/' + this.folder + '/' + '_' + this._.slugify(this.name.toLowerCase()) + '.less');
+        }
+        else if (this.cssOption === 'SCSS') {
+            this.template('style.less', 'dev/styles/' + this.folder + '/' + '_' + this._.slugify(this.name.toLowerCase()) + '.scss');
+        }
+    }
+    else {
+        if (this.cssOption === 'LESS') {
+            this.template('style.less', 'dev/styles/partials/' + '_' + this._.slugify(this.name.toLowerCase()) + '.less');
+        }
+        else if (this.cssOption === 'SCSS') {
+            this.template('style.less', 'dev/styles/partials/' + '_' + this._.slugify(this.name.toLowerCase()) + '.scss');
+        }
+    }
+};
