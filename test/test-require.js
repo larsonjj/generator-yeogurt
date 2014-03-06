ad4203082ed714d710c6fce2278114fe6d8458cc
Jake Larson <jake.j.larson@gmail.com>
2014-03-05 21:12:16 -0600

    Added test for RequireJS option

---
+++ test/test-require.js
@@ -0,0 +1,47 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var path    = require('path');
+var helpers = require('yeoman-generator').test;
+
+
+describe('yeogurt generator svn', function () {
+    beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+            if (err) {
+                return done(err);
+            }
+
+            this.app = helpers.createGenerator('yeogurt:app', [
+                '../../app'
+            ]);
+            done();
+        }.bind(this));
+    });
+
+    it('creates expected files', function (done) {
+        var expected = [
+            // add files and folders you expect to exist here.
+            'test/test-main.js'
+        ];
+
+        helpers.mockPrompt(this.app, {
+            projectName: 'testing',
+            versionControl: 'SVN',
+            cssOption: 'SCSS',
+            jsOption: 'RequireJS',
+            ieSupport: true,
+            responsive: true,
+            useGA: true,
+            useFTP: true,
+            jshint: true,
+            useDashboard: true,
+            extras: ['htaccess', 'useBootstrap', 'useFontAwesome', 'ieIcons', 'adobeXdomain', 'appleIcon']
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+            helpers.assertFile(expected);
+            done();
+        });
+    });
+});
