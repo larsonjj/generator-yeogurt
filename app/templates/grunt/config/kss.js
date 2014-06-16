dba2736c763ba121c763b68a6b574cee7a3ebe98
Mark Brouch <mark.brouch@gmail.com>
2014-06-16 14:04:39 -0500

    Sandboxing site styles from styleguide styles

--- app/templates/grunt/config/kss.js
+++ app/templates/grunt/config/kss.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
     grunt.config.set('kss', {
         options: {
             includeType: '<% if (cssOption === 'SASS') { %>scss<% } else if (cssOption === 'LESS') { %>less<% } else if (cssOption === 'None (Vanilla CSS)') { %>css<% } %>',
-            includePath: '<%%= yeogurt.dev %>/styles/main.<% if (cssOption === 'SASS') { %>scss<% } else if (cssOption === 'LESS') { %>less<% } else if (cssOption === 'None (Vanilla CSS)') { %>css<% } %>',<% if (cssOption === 'SASS') { %>
+            includePath: '<%%= yeogurt.dev %>/docs/styleguide.<% if (cssOption === 'SASS') { %>scss<% } else if (cssOption === 'LESS') { %>less<% } else if (cssOption === 'None (Vanilla CSS)') { %>css<% } %>',<% if (cssOption === 'SASS') { %>
             scssRoot: ['<%%= yeogurt.dev %>/styles'],<% } %>
             template: '<%%= yeogurt.dev %>/docs/styleguide'
         },
