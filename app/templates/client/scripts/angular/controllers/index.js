b5aade76a80ca3d0bc273fd834f8a69c92818f16
Jake Larson <jake.j.larson@gmail.com>
2015-01-11 04:16:30 -0600

    Fixed module loading issues within the index controller to angular apps

--- app/templates/client/scripts/angular/controllers/index.js
+++ app/templates/client/scripts/angular/controllers/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-angular.module('<%= _.classify(projectName) %>.index', ['ngRoute'])
+angular.module('<%= _.classify(projectName) %>', [
+    '<%= _.classify(projectName) %>.index'
+])
 
 .controller('IndexCtrl', [function() {
 
