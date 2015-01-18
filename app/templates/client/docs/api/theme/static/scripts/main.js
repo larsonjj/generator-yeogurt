12c2439f7d07f7f88bdcd2478873591d40fe125a
Jake Larson <jake.j.larson@gmail.com>
2015-01-18 02:16:57 -0600

    Updated indent from 4 spaces to 2 for docs folder

--- app/templates/client/docs/api/theme/static/scripts/main.js
+++ app/templates/client/docs/api/theme/static/scripts/main.js
@@ -1,11 +1,11 @@
 /*global $: true*/
 /*global prettyPrint: true*/
 $(function() {
-    'use strict';
-    prettyPrint();
-    var $button = $( '.api-nav-toggle' );
-    $button.on('click', function() {
-        $(this).toggleClass( 'active' );
-        $(' .navigation ').toggleClass('open');
-    });
+  'use strict';
+  prettyPrint();
+  var $button = $( '.api-nav-toggle' );
+  $button.on('click', function() {
+    $(this).toggleClass( 'active' );
+    $(' .navigation ').toggleClass('open');
+  });
 });
--- app/templates/client/docs/dashboard/scripts/main.js
+++ app/templates/client/docs/dashboard/scripts/main.js
@@ -3,67 +3,67 @@
 'use strict';
 $(function() {
 
-    var $itemItems = $('.item-list li');
-    var $categoryItems = $('.category li');
-    var $legendItems = $('.legend li');
+  var $itemItems = $('.item-list li');
+  var $categoryItems = $('.category li');
+  var $legendItems = $('.legend li');
 
-    var $clearButtons = $('.filter-clear');
+  var $clearButtons = $('.filter-clear');
 
-    var $sidebar = $('.sidebar');
-    var $menuButton = $('.dash-button-wrapper');
-    var isOpen = false;
+  var $sidebar = $('.sidebar');
+  var $menuButton = $('.dash-button-wrapper');
+  var isOpen = false;
 
-    var changeActiveItem = function(items, theClass, $context) {
-      items.removeClass(theClass);
-      $context.addClass(theClass);
-    };
+  var changeActiveItem = function(items, theClass, $context) {
+    items.removeClass(theClass);
+    $context.addClass(theClass);
+  };
 
-    var clearFilters = function($context) {
-      $context.addClass('hide');
-      var $listItems = $context.parent().find('li');
-      $listItems.removeClass('active');
-    };
+  var clearFilters = function($context) {
+    $context.addClass('hide');
+    var $listItems = $context.parent().find('li');
+    $listItems.removeClass('active');
+  };
 
-    var showClearButton = function($context) {
-      $context.parent().parent().find('.filter-clear').removeClass('hide');
-    };
+  var showClearButton = function($context) {
+    $context.parent().parent().find('.filter-clear').removeClass('hide');
+  };
 
-    var filterItems = function() {
-      var $categories = $('.sidebar .active').map(function(i, item) {return $(item).data('category');});
-      $itemItems.addClass('hide');
-      $itemItems.filter('.' + $categories.get().join('.')).removeClass('hide');
-    };
+  var filterItems = function() {
+    var $categories = $('.sidebar .active').map(function(i, item) {return $(item).data('category');});
+    $itemItems.addClass('hide');
+    $itemItems.filter('.' + $categories.get().join('.')).removeClass('hide');
+  };
 
-    var catItemsClick = function() {
-        changeActiveItem($categoryItems, 'active', $(this));
-        filterItems();
-    };
+  var catItemsClick = function() {
+    changeActiveItem($categoryItems, 'active', $(this));
+    filterItems();
+  };
 
-    var legendItemsClick = function() {
-        changeActiveItem($legendItems, 'active', $(this));
-        showClearButton($(this));
-        filterItems();
-    };
+  var legendItemsClick = function() {
+    changeActiveItem($legendItems, 'active', $(this));
+    showClearButton($(this));
+    filterItems();
+  };
 
-    var clearButtonsClick = function() {
-        clearFilters($(this));
-        filterItems();
-    };
+  var clearButtonsClick = function() {
+    clearFilters($(this));
+    filterItems();
+  };
 
-    var menuButtonClick = function() {
-        if (!isOpen) {
-            isOpen = true;
-            $sidebar.addClass('open');
-        }
-        else {
-            isOpen = false;
-            $sidebar.removeClass('open');
-        }
-    };
+  var menuButtonClick = function() {
+    if (!isOpen) {
+      isOpen = true;
+      $sidebar.addClass('open');
+    }
+    else {
+      isOpen = false;
+      $sidebar.removeClass('open');
+    }
+  };
 
-    $categoryItems.on('click', catItemsClick);
-    $legendItems.on('click', legendItemsClick);
-    $clearButtons.on('click', clearButtonsClick);
-    $menuButton.on('click', menuButtonClick);
+  $categoryItems.on('click', catItemsClick);
+  $legendItems.on('click', legendItemsClick);
+  $clearButtons.on('click', clearButtonsClick);
+  $menuButton.on('click', menuButtonClick);
 
 });
