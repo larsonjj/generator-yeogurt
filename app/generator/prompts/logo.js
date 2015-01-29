2911d043d4691bc8ba83166925180263d30a0046
Jake Larson <jake.j.larson@gmail.com>
2015-01-28 23:57:23 -0600

    Fixed serverTesting prompt Fixed logo

--- app/generator/prompts/logo.js
+++ app/generator/prompts/logo.js
@@ -6,14 +6,14 @@
 
 var logoPrompt = function logoPrompt() {
   var yeogurtLogo = '' +
-    '                  _   \n'.red +
-    '  ' + 'Welcome to'.green + '             | |  \n'.red +
+    '                                    _   \n'.red +
+    '  ' + 'Welcome to'.green + '                       | |  \n'.red +
     '  _   _  ___  ___   __ _ _   _ _ __| |_ \n'.red +
     ' | | | |/ _ \\\/ _ \\ \/ _` | | | | \'__| __|\n'.red +
     ' | |_| |  __/ (_) | (_| | |_| | |  | |_ \n'.red +
     '  \\__, |\\___|\\___/ \\__, |\\__,_|_|   \\__|\n'.red +
-    '   __/ |      __/ |\n'.red +
-    '  |___/      |___/  '.red +
+    '   __/ |            __/ |\n'.red +
+    '  |___/            |___/  '.red +
     '               \n';
 
   // have Yeogurt greet the user.
