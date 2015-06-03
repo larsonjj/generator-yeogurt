0add80a157bbeaae096651cf5381946be3eb98e9
Jake Larson <jake.j.larson@gmail.com>
2015-06-03 17:14:16 -0500

    Removed markup example in module dashboard

--- app/generator/files/docs.js
+++ app/generator/files/docs.js
@@ -8,10 +8,10 @@ var docFiles = function docFiles() {
 
   if (this.useDashboard) {
     this.template('src/docs/dashboard/styles/dashboard.less', 'src/_docs/dashboard/styles/dashboard.less');
-    this.template('src/docs/dashboard/styles/module-syntax.less', 'src/_docs/dashboard/styles/module-syntax.less');
+    // this.template('src/docs/dashboard/styles/module-syntax.less', 'src/_docs/dashboard/styles/module-syntax.less');
     this.template('src/docs/dashboard/scripts/less.js', 'src/_docs/dashboard/scripts/less.js');
     this.template('src/docs/dashboard/scripts/jquery.js', 'src/_docs/dashboard/scripts/jquery.js');
-    this.template('src/docs/dashboard/scripts/highlight.js', 'src/_docs/dashboard/scripts/highlight.js');
+    // this.template('src/docs/dashboard/scripts/highlight.js', 'src/_docs/dashboard/scripts/highlight.js');
     this.template('src/docs/dashboard/scripts/main.js', 'src/_docs/dashboard/scripts/main.js');
     this.copy('src/docs/dashboard/template.hbs', 'src/_docs/dashboard/template.hbs');
     this.copy('src/docs/dashboard/module-template.hbs', 'src/_docs/dashboard/module-template.hbs');
