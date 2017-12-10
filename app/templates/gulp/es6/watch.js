cb6e007fa1415e079421d0659bfd02d53986d7cc
Daniel Tonon <daniel.tonon@adelphi.digital>
2017-12-10 13:03:11 +1100

    Updated watch functions to be Gulp 4 compatible
    I forgot to do this in the original gulp 4 upgrade commit. The watch task should work with Gulp 4 now.

--- app/templates/gulp/es6/watch.js
+++ app/templates/gulp/es6/watch.js
@@ -8,32 +8,33 @@ export default function(gulp, plugins, args, config, taskTarget, browserSync) {
   // Watch task
   gulp.task('watch', (done) => {
     if (!args.production) {<% if (cssOption === 'sass') { %>
+
       // Styles
       gulp.watch([
         path.join(dirs.source, dirs.styles, '**/*.{scss,sass}'),
         path.join(dirs.source, dirs.modules, '**/*.{scss,sass}')
-      ], ['sass']);<% } else if (cssOption === 'less') { %>
+      ], gulp.series('sass'));<% } else if (cssOption === 'less') { %>
       gulp.watch([
         path.join(dirs.source, dirs.styles, '**/*.less'),
         path.join(dirs.source, dirs.modules, '**/*.less'),
-      ], ['less']);<% } else if (cssOption === 'stylus') { %>
+      ], gulp.series('less'));<% } else if (cssOption === 'stylus') { %>
       gulp.watch([
         path.join(dirs.source, dirs.styles, '**/*.styl'),
         path.join(dirs.source, dirs.modules, '**/*.styl')
-      ], ['stylus']);
+      ], gulp.series('stylus'));
       <% } %><% if (htmlOption === 'jade') { %>
 
       // Jade Templates
       gulp.watch([
         path.join(dirs.source, '**/*.jade'),
         path.join(dirs.source, dirs.data, '**/*.{json,yaml,yml}')
-      ], ['jade']);<% } else if (htmlOption === 'nunjucks') { %>
+      ], gulp.series('jade'));<% } else if (htmlOption === 'nunjucks') { %>
 
       // Nunjucks Templates
       gulp.watch([
         path.join(dirs.source, '**/*.nunjucks'),
         path.join(dirs.source, dirs.data, '**/*.{json,yaml,yml}')
-      ], ['nunjucks']);
+      ], gulp.series('nunjucks'));
       <% } %>
 
       // Copy
@@ -42,12 +43,12 @@ export default function(gulp, plugins, args, config, taskTarget, browserSync) {
         '!' + path.join(dirs.source, '{**/\_*,**/\_*/**}')<% if (htmlOption === 'nunjucks') { %>,
         '!' + path.join(dirs.source, '**/*.nunjucks')<% } else if (htmlOption === 'jade') { %>,
         '!' + path.join(dirs.source, '**/*.jade')<% } %>
-      ], ['copy']);
+      ], gulp.series('copy'));
 
       // Images
       gulp.watch([
         path.join(dirs.source, dirs.images, '**/*.{jpg,jpeg,gif,svg,png}')
-      ], ['imagemin']);
+      ], gulp.series('imagemin'));
 
       // All other files
       gulp.watch([
