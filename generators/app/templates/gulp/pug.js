1abb6f6bb04f1c6a870826ae0b263cb6253f5df0
Jake Larson <jake.j.larson@gmail.com>
2019-08-04 00:29:24 -0500

    Fix pug

--- generators/app/templates/gulp/pug.js
+++ generators/app/templates/gulp/pug.js
@@ -18,18 +18,16 @@ export default function(gulp, plugins, args, config, taskTarget, browserSync) {
       // Convert directory to JS Object
       siteData = foldero(dataPath, {
         recurse: true,
-        whitelist: '(.*/)*.+\.(json|ya?ml)$',
+        whitelist: '(.*/)*.+.(json|ya?ml)$',
         loader: function loadAsString(file) {
           let json = {};
           try {
             if (path.extname(file).match(/^.ya?ml$/)) {
               json = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
-            }
-            else {
+            } else {
               json = JSON.parse(fs.readFileSync(file, 'utf8'));
             }
-          }
-          catch(e) {
+          } catch (e) {
             console.log('Error Parsing DATA file: ' + file);
             console.log('==== Details Below ====');
             console.log(e);
@@ -44,35 +42,42 @@ export default function(gulp, plugins, args, config, taskTarget, browserSync) {
     if (args.debug) {
       console.log('==== DEBUG: site.data being injected to templates ====');
       console.log(siteData);
-      console.log('\n==== DEBUG: package.json config being injected to templates ====');
+      console.log(
+        '\n==== DEBUG: package.json config being injected to templates ===='
+      );
       console.log(config);
     }
 
-    return gulp.src([
-      path.join(dirs.source, '**/*.pug'),
-      '!' + path.join(dirs.source, '{**/\_*,**/\_*/**}')
-    ])
-    .pipe(plugins.changed(dest))
-    .pipe(plugins.plumber())
-    .pipe(plugins.pug({
-      pug: pug,
-      pretty: true,
-      locals: {
-        config: config,
-        debug: true,
-        site: {
-          data: siteData
-        }
-      }
-    }))
-    .pipe(plugins.htmlmin({
-      collapseBooleanAttributes: true,
-      conservativeCollapse: true,
-      removeCommentsFromCDATA: true,
-      removeEmptyAttributes: true,
-      removeRedundantAttributes: true
-    }))
-    .pipe(gulp.dest(dest))
-    .on('end', browserSync.reload);
+    return gulp
+      .src([
+        path.join(dirs.source, '**/*.pug'),
+        '!' + path.join(dirs.source, '{**/_*,**/_*/**}')
+      ])
+      .pipe(plugins.changed(dest))
+      .pipe(plugins.plumber())
+      .pipe(
+        plugins.pug({
+          pug: pug,
+          pretty: true,
+          locals: {
+            config: config,
+            debug: true,
+            site: {
+              data: siteData
+            }
+          }
+        })
+      )
+      .pipe(
+        plugins.htmlmin({
+          collapseBooleanAttributes: true,
+          conservativeCollapse: true,
+          removeCommentsFromCDATA: true,
+          removeEmptyAttributes: true,
+          removeRedundantAttributes: true
+        })
+      )
+      .pipe(gulp.dest(dest))
+      .on('end', browserSync.reload);
   });
 }
