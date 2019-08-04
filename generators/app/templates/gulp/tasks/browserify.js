b39393096180af586edf1847cbd4856548ee3595
Daniel Tonon <daniel.tonon.503@gmail.com>
2019-08-04 21:19:53 +1000

    Using native Object.assign() instead of lodash
    There is no need to load in Lodash here when Object.assign() is a native way to do the exact same thing.

--- generators/app/templates/gulp/tasks/browserify.js
+++ generators/app/templates/gulp/tasks/browserify.js
@@ -8,7 +8,6 @@ import browserify from 'browserify';
 import watchify from 'watchify';
 import envify from 'envify';
 import babelify from 'babelify';
-import _ from 'lodash';
 import vsource from 'vinyl-source-stream';
 import buffer from 'vinyl-buffer';
 import gulpif from 'gulp-if';
@@ -31,7 +30,7 @@ let browserifyTask = (files, done) => {
 
     if (!args.production) {
       // Setup Watchify for faster builds
-      let opts = _.assign({}, watchify.args, customOpts);
+      let opts = Object.assign({}, watchify.args, customOpts);
       bundler = watchify(browserify(opts));
     }
 
